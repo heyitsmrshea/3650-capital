8467bdd51d49691d8a5f46b459d8d88bee045d5e
Andrew Shea <ashea951@gmail.com>
2026-03-10 07:16:51 -0500

    Mirror 3650 Ideas Loop exactly

--- leadership-data.js
+++ leadership-data.js
@@ -2,14 +2,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Corey Altman",
     role: "Senior Associate",
-    image: "./assets/images/3650-0608-Edit-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-0608-Edit-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Daniel Antonelli",
     role: "Managing Director - Head of Asset Management & Servicing",
-    image: "./assets/images/Dan-.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/06/Dan-.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/daniel-antonelli/?portfolioCats=22",
     bio: [
       "Mr. Antonelli oversees the Miami-based Commercial Loan Servicing and Asset Management platforms for 3650 Capital and its affiliate Grass River Property and brings 15 years of commercial real estate, loan asset management and loan servicing experience. Mr. Antonelli has been with the firm since 2014 and during his tenure he and his team have managed over $17.5 billion in commercial real estate loans and real estate investments across all geographies and asset types, with diverse and complex capital and investment structures and related operational and administration responsibilities.",
@@ -19,7 +19,7 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Malay Bansal",
     role: "Managing Partner, Head of Trading & Capital Markets",
-    image: "./assets/images/3650-Headshots-A0385.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/3650-Headshots-A0385.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/malay-bansal/?portfolioCats=22",
     bio: [
       "Mr. Bansal is 3650 Capital's Head of Trading. Mr. Bansal brings over 30 years of broad markets experience in commercial real estate, capital markets, and technology. Roles he has held include head of trading, head of securitization/banking, head of CRE warehouse financing, head of structuring, and head of portfolio management and advisory.",
@@ -31,14 +31,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Alexandria Bierstedt",
     role: "AP Specialist",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A1570-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A1570-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Noelle Bowman",
     role: "Director of Human Resources",
-    image: "./assets/images/3650-2987-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/10/3650-2987-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/noelle-bowman/?portfolioCats=22",
     bio: [
       "Ms. Bowman oversees Human Resources for 3650 Capital and brings over 20 years’ experience in commercial real estate development, property management and operations.",
@@ -50,14 +50,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Tess Campbell",
     role: "Analyst",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A0994_2-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A0994_2-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Toby Cobb",
     role: "Managing Partner, Co-Founder",
-    image: "./assets/images/3650-Headshots-A0746.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/3650-Headshots-A0746.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/toby-cobb/?portfolioCats=22",
     bio: [
       "Toby Cobb, Co-Founder and Managing Partner of 3650 Capital, has 30 years of experience in commercial real estate and finance. In 2013, Mr. Cobb and his partner Justin Kennedy co-founded 3650 Capital's CRE equity and development affiliate, Grass River Property, a leading South Florida developer with approximately 2 million square feet of real estate owned.",
@@ -68,14 +68,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Natalie DeLacio, CPA",
     role: "Vice President & Assistant Controller",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A0667-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A0667-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Ken Dickey",
     role: "Managing Partner, Head of Fixed Rate Lending",
-    image: "./assets/images/3650-Headshots-A0088-2.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/3650-Headshots-A0088-2.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/ken-dickey/?portfolioCats=22",
     bio: [
       "Mr. Dickey has 26 years of CRE lending and relationship management experience and leads the 3650 Capital fixed rate originations platform. Previously, Mr. Dickey was Managing Director and led origination for the CRE lending platform at The Bank of New York Mellon (“BONY”). Prior to BONY, Mr. Dickey served as President of Deutsche Bank Mortgage Capital where he worked together with Kennedy and Cobb for more than 10 years.",
@@ -86,14 +86,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Jorge Espinal",
     role: "Vice President",
-    image: "./assets/images/3650-0879-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-0879-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Julia Ferrier",
     role: "Senior Associate",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A0638-2-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/10/3650-Headshots-Oct-2023-Miami-A0638-2-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/julia-kharzeev/?portfolioCats=22",
     bio: [
       "Julia Ferrier is a Senior Associate in the Investment Management division of 3650 Capital. Julia has been with 3650 since 2021.",
@@ -104,7 +104,7 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Michael Fleischer",
     role: "Deputy Chief Investment Officer & Head of Real Estate Credit Solutions",
-    image: "./assets/images/MichaelF-04688A3.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/09/MichaelF-04688A3.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/michael-fleischer/?portfolioCats=22",
     bio: [
       "Mr. Fleischer has 20 years of commercial real estate experience and leads 3650 Capital's Real Estate Credit Solutions. Previously, Mr. Fleischer was a Managing Director with RHRECS, a debt fund co-managed by Related Fund Management and HPS Investment Partners where he focused on transaction origination, structuring, asset management and portfolio finance.",
@@ -115,14 +115,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Raelene Vepuri",
     role: "Senior Associate",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A0731-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A0731-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Andre Gonzalez",
     role: "Managing Director",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A0073-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A0073-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/andre-gonzalez/?portfolioCats=22",
     bio: [
       "Andre Gonzalez is a Managing Director at 3650 Capital where he focuses on capital formation, investor relations, and strategic growth initiatives including M&A. Andre was previously a Director at InvestRes, a real estate private equity firm with $1.5 billion in assets under management. Andre graduated with a BA from Harvard University."
@@ -131,21 +131,21 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Barrett Graham",
     role: "Associate",
-    image: "./assets/images/MGM_2579-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/MGM_2579-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Alvaro Guerra",
     role: "Controller",
-    image: "./assets/images/3650-1039-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-1039-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Evan Guttman",
     role: "Director of Technology",
-    image: "./assets/images/3650-Headshots-Oct-2023-NYC-B1181-3-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-NYC-B1181-3-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/evan-guttman/?portfolioCats=22",
     bio: [
       "Evan Guttman has over two decades of experience working in IT on Wall Street. Mr. Guttman is responsible for the technology at 3650 Capital as well as managing the firm’s cybersecurity and data.",
@@ -155,21 +155,21 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Ryan Hendriks",
     role: "Vice President",
-    image: "./assets/images/3650-1269-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-1269-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Axl Holmes",
     role: "Analyst",
-    image: "./assets/images/Axl-Homes-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/Axl-Homes-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Mark Jefferis",
     role: "Managing Partner, Chief Administrative Officer, and General Counsel",
-    image: "./assets/images/Mark-Jefferis-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/Mark-Jefferis-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/mark-jefferis/?portfolioCats=22",
     bio: [
       "Mr. Jefferis is 3650 Capital’s General Counsel and Chief Compliance Officer. Mr. Jefferis brings 16 years of commercial real estate investment, capital markets, and other legal experience to 3650 Capital.",
@@ -181,7 +181,7 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Justin Kennedy",
     role: "Managing Partner, Co-Founder",
-    image: "./assets/images/3650-Headshots-A1067.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/3650-Headshots-A1067.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/justin-kennedy/?portfolioCats=22",
     bio: [
       "3650 Capital Co-Founder Justin Kennedy brings 32 years of CRE experience, including finance, distressed assets and development. In 2013, Kennedy and Cobb co-founded 3650 Capital’s CRE development affiliate, Grass River Property, a leading South Florida developer and manager of mixed-use, urban-infill and transit-oriented properties. In partnership with leading institutional partners, the Grass River portfolio includes 14 development and equity investments totaling more than 1.8 million SF.",
@@ -192,14 +192,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Michael Kundin",
     role: "Analyst",
-    image: "./assets/images/3650-2750-Edit-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-2750-Edit-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Peter LaPointe",
     role: "Managing Partner, Chief Operating Officer",
-    image: "./assets/images/Peter-3650-A013.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/Peter-3650-A013.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/peter-lapointe/?portfolioCats=22",
     bio: [
       "Mr. LaPointe has 21 years of experience in CRE loan origination and servicing, development, and distressed assets acquisitions and management. In 2013, Mr. LaPointe joined Cobb and Kennedy during the formation of 3650 Capital’s affiliate, Grass River Property.",
@@ -210,14 +210,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Johanna Maranon",
     role: "Vice President, Asset Management",
-    image: "./assets/images/3650-0657-Edit-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-0657-Edit-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Michael McCabe",
     role: "Director, Originations",
-    image: "./assets/images/3650-Headshots-Oct-2023-NYC-B1078-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/12/3650-Headshots-Oct-2023-NYC-B1078-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/michael-mccabe/?portfolioCats=22",
     bio: [
       "Mr. McCabe is a Director at 3650 Capital, responsible for the origination, structuring, and execution of lending opportunities on all property types nationwide. Mr. McCabe has over 12 years of experience in the commercial real estate investment and mortgage lending business and was part of the original 3650 fixed rate lending team launched in 2017.",
@@ -228,14 +228,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Noah Moghavem",
     role: "Associate",
-    image: "./assets/images/Noah-Moghavem-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/Noah-Moghavem-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Andrew Parower",
     role: "Managing Partner, Chief Credit Officer",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A0562-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/12/3650-Headshots-Oct-2023-Miami-A0562-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/andrew-parower/?portfolioCats=22",
     bio: [
       "Mr. Parower is responsible for analyzing lending opportunities and general loan-portfolio management. Mr. Parower brings 29 years of commercial real estate investment, capital markets, and fund management experience to 3650 Capital. Prior to 3650 REIT, Andrew was Managing Director of Marketfield Asset Management where he launched and managed a $300 million CMBS fund in 2009.",
@@ -247,7 +247,7 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Will Pierce",
     role: "Director",
-    image: "./assets/images/151A8204.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/151A8204.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/will-pierce/?portfolioCats=22",
     bio: [
       "Mr. Pierce, Director, has 7 years of commercial real estate investment experience and works within the 3650 Capital Bridge and Event Driven lending platform. In 2014, Mr. Pierce joined 3650 Capital’s CRE equity and development affiliate, Grass River Property, a leading South Florida developer with approximately 2 million sq feet of real estate owned.",
@@ -257,14 +257,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Richard Putnam",
     role: "Vice President, Originations",
-    image: "./assets/images/MGM_2584-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/MGM_2584-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Alannah Rabich",
     role: "Vice President",
-    image: "./assets/images/3650-0507-Edit-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/10/3650-0507-Edit-scaled.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/alannah-rabich/?portfolioCats=22",
     bio: [
       "Alannah Rabich is a Vice President at 3650 Capital where she serves the Investor Relations department. Ms. Rabich has been with 3650 Capital since 2021.",
@@ -275,21 +275,21 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Agne Rader",
     role: "Executive Assistant",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A2125-2-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A2125-2-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Martin Rivas, CPA",
     role: "Chief Accounting Officer",
-    image: "./assets/images/3650-Headshots-Oct-2023-Miami-A1141-2-2-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-Headshots-Oct-2023-Miami-A1141-2-2-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Jonathan Roth",
     role: "Managing Partner, Co-Founder",
-    image: "./assets/images/3650-Headshots-A0857.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2020/08/3650-Headshots-A0857.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/jonathan-roth/?portfolioCats=22",
     bio: [
       "Mr. Roth, Co-Founder, brings 30 years of experience originating, structuring and servicing CRE investments. Previously, Mr. Roth served as President of Canyon Partners Real Estate LLC. During his 18-year tenure at Canyon, Mr. Roth was instrumental in the formation of several investment vehicles which collectively invested approximately $5.6 billion in over 200 transactions.",
@@ -300,28 +300,28 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Stephanie Sabellico",
     role: "Executive Assistant",
-    image: "./assets/images/EVN0949-Edit-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/EVN0949-Edit-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Jeremiah Sharf",
     role: "Vice President",
-    image: "./assets/images/Jeremiah-Sharf_1-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/Jeremiah-Sharf_1-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Katie Spillman",
     role: "Executive Assistant",
-    image: "./assets/images/Katie-Spillman-1-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/Katie-Spillman-1-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Jake Stahler",
     role: "Managing Director, Co Head of Fixed Rate Lending",
-    image: "./assets/images/3650-Headshots-A0720-min.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/01/3650-Headshots-A0720-min.jpg",
     bioUrl: "https://3650capital.com/portfolio-item/jake-stahler/?portfolioCats=22",
     bio: [
       "Mr. Stahler has over 23 years of commercial real estate experience and is based in 3650’s New York office. He is responsible for national originations and the operations of the SCF business. Over his career, Mr. Stahler has originated over $5 billion commercial mortgage loans throughout the United States. Mr. Stahler was an original member of the fixed rate lending team at 3650. Prior to 3650, Mr. Stahler worked for the Bank of New York’s capital markets lending program, Deutsche Bank, and Gramercy Capital Corp. Mr. Stahler holds a B.A. in History from Colgate University."
@@ -330,14 +330,14 @@ window.LEADERSHIP_MEMBERS = [
   {
     name: "Brent Thurn",
     role: "Director",
-    image: "./assets/images/3650-2564-scaled.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/11/3650-2564-scaled.jpg",
     bioUrl: null,
     bio: []
   },
   {
     name: "Daniel Vaknin",
     role: "Senior Associate",
-    image: "./assets/images/3650-1192-Edit.jpg",
+    image: "https://3650capital.com/wp-content/uploads/2023/10/3650-1192-Edit.jpg",
     bioUrl: null,
     bio: []
   }
